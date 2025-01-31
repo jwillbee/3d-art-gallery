e4046b2643cdd18fcbaf9e9a91cf357dcb28727c
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-01-31 08:45:15 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-
-function Box() {
-  return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  );
-}
+import Gallery from "./Gallery";
 
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas camera={{ position: [0, 0, 5] }}>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[2, 2, 2]} />
-        <Box />
-        <OrbitControls />
-      </Canvas>
+      <Gallery />
     </div>
   );
 }
