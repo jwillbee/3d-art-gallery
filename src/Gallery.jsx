9dfc0556b10d8dc6ad0ac2b6d4f05de0bf059f7e
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-01-31 14:13:59 -0500

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,17 +2,9 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
-function DebugBox() {
+function ArtFrame({ position, rotation }) {
   return (
-    <mesh position={[0, 1, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="red" />
-    </mesh>
-  );
-}
-function ArtFrame({ position }) {
-  return (
-    <mesh position={position}>
+    <mesh position={position} rotation={rotation}>
       <boxGeometry args={[1.5, 1, 0.1]} />
       <meshStandardMaterial color="black" />
     </mesh>
@@ -22,44 +14,44 @@ function ArtFrame({ position }) {
 function GalleryRoom() {
   return (
     <group>
-      {/* 🟦 Floor */}
-      <mesh position={[0, -0.5, 0]} receiveShadow>
+      {/* 🟦 Floor (Adjusted Walls to Remove Gap) */}
+      <mesh position={[0, -0.05, 0]} receiveShadow>
         <boxGeometry args={[10, 0.1, 10]} />
         <meshStandardMaterial color="gray" />
       </mesh>
 
-      {/* 🟩 Left Wall */}
-      <mesh position={[-5, 2, 0]} receiveShadow>
+      {/* 🟩 Left Wall (Lowered by 0.05) */}
+      <mesh position={[-5, 2, 0]} rotation={[0, Math.PI / 2, 0]} receiveShadow>
         <boxGeometry args={[0.1, 4, 10]} />
         <meshStandardMaterial color="white" />
       </mesh>
 
-      {/* 🟩 Right Wall */}
-      <mesh position={[5, 2, 0]} receiveShadow>
+      {/* 🟩 Right Wall (Lowered by 0.05) */}
+      <mesh position={[5, 2, 0]} rotation={[0, -Math.PI / 2, 0]} receiveShadow>
         <boxGeometry args={[0.1, 4, 10]} />
         <meshStandardMaterial color="white" />
       </mesh>
 
-      {/* 🟦 Back Wall */}
+      {/* 🟦 Back Wall (Lowered by 0.05) */}
       <mesh position={[0, 2, -5]} receiveShadow>
         <boxGeometry args={[10, 4, 0.1]} />
         <meshStandardMaterial color="white" />
       </mesh>
 
-            {/* 🎨 Art Frames on Left Wall */}
-      <ArtFrame position={[-4.9, 2, -2]} />
-      <ArtFrame position={[-4.9, 2, 2]} />
+      {/* 🎨 Art Frames on Left Wall (Rotated to Face Inward) */}
+      <ArtFrame position={[-4.9, 2, -2]} rotation={[0, Math.PI / 2, 0]} />
+      <ArtFrame position={[-4.9, 2, 2]} rotation={[0, Math.PI / 2, 0]} />
 
-      {/* 🎨 Art Frames on Right Wall */}
-      <ArtFrame position={[4.9, 2, -2]} />
-      <ArtFrame position={[4.9, 2, 2]} />
+      {/* 🎨 Art Frames on Right Wall (Rotated to Face Inward) */}
+      <ArtFrame position={[4.9, 2, -2]} rotation={[0, -Math.PI / 2, 0]} />
+      <ArtFrame position={[4.9, 2, 2]} rotation={[0, -Math.PI / 2, 0]} />
 
-      {/* 🎨 Art Frames on Back Wall */}
+      {/* 🎨 Art Frames on Back Wall (No Rotation Needed) */}
       <ArtFrame position={[0, 2, -4.9]} />
       <ArtFrame position={[2, 2, -4.9]} />
       <ArtFrame position={[-2, 2, -4.9]} />
 
-      {/* 🔆 Better Lighting */}
+      {/* 🔆 Lighting */}
       <pointLight position={[0, 3, 0]} intensity={2} castShadow />
     </group>
   );
@@ -67,12 +59,9 @@ function GalleryRoom() {
 
 export default function Gallery() {
   return (
-    <Canvas shadows camera={{ position: [0, 2, 6], fov: 50 }}>
+    <Canvas camera={{ position: [0, 2, 6] }}>
       <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} castShadow />
-      
-      <DebugBox /> {/* 🔴 Red test box should appear */}
-
+      <directionalLight position={[2, 5, 2]} />
       <GalleryRoom />
       <OrbitControls />
     </Canvas>
