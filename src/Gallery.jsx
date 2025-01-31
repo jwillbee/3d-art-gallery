6cd40bd993e7d28bb8ec75e0e61e973f778ef769
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-01-31 14:32:15 -0500

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,76 +1,21 @@
 import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { useSpring, animated } from "@react-spring/three";
-
-function Room({ position, isActive, onClick, roomName }) {
-  const { position: animatedPos } = useSpring({
-    position: isActive ? [0, 0, 0] : position,
-    config: { mass: 1, tension: 170, friction: 26 },
-  });
-
-  return (
-    <animated.group position={animatedPos} onClick={onClick}>
-      {/* Floor */}
-      <mesh position={[0, -0.5, 0]}>
-        <boxGeometry args={[4, 0.1, 4]} />
-        <meshStandardMaterial color="gray" />
-      </mesh>
-
-      {/* Walls */}
-      <mesh position={[0, 1, -2]}>
-        <boxGeometry args={[4, 2, 0.1]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      <mesh position={[2, 1, 0]} rotation={[0, Math.PI / 2, 0]}>
-        <boxGeometry args={[4, 2, 0.1]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      <mesh position={[-2, 1, 0]} rotation={[0, Math.PI / 2, 0]}>
-        <boxGeometry args={[4, 2, 0.1]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-
-      {/* Doorway */}
-      <mesh position={[0, 1, 2]}>
-        <boxGeometry args={[1.5, 2, 0.1]} />
-        <meshStandardMaterial color="white" transparent opacity={0.2} />
-      </mesh>
-
-      {/* Room Sign */}
-      <mesh position={[0, 2, 2]}>
-        <textGeometry args={[roomName, { size: 1, height: 0.1 }]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
-    </animated.group>
-  );
-}
 
 export default function Gallery() {
-  const [activeRoom, setActiveRoom] = useState(0);
-  const rooms = [
-    { position: [0, 0, 0], roomName: "Main Room" },
-    { position: [4, 0, 0], roomName: "Room 2" },
-    { position: [-4, 0, 0], roomName: "Room 3" },
-  ];
-
   return (
     <Canvas camera={{ position: [0, 2, 5] }}>
+      {/* Add light source */}
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} />
 
-      {/* Render Rooms */}
-      {rooms.map((room, index) => (
-        <Room
-          key={index}
-          position={room.position}
-          isActive={index === activeRoom}
-          onClick={() => setActiveRoom(index)}
-          roomName={room.roomName}
-        />
-      ))}
+      {/* A simple cube */}
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
 
-      {/* OrbitControls for navigating around */}
+      {/* OrbitControls for camera movement */}
       <OrbitControls />
     </Canvas>
   );
