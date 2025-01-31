c18156313467ee664903356370d186366b99488f
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-01-31 14:30:51 -0500

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,8 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { useSpring, animated } from "@react-spring/three";
 
-// Room component with art and sign
-function Room({ position, isActive, onClick, artFrames, roomName }) {
+function Room({ position, isActive, onClick, roomName }) {
   const { position: animatedPos } = useSpring({
     position: isActive ? [0, 0, 0] : position,
     config: { mass: 1, tension: 170, friction: 26 },
@@ -38,15 +37,6 @@ function Room({ position, isActive, onClick, artFrames, roomName }) {
         <meshStandardMaterial color="white" transparent opacity={0.2} />
       </mesh>
 
-      {/* Artframes */}
-      {artFrames.map((frame, index) => (
-        <mesh key={index} position={frame.position}>
-          <planeGeometry args={[2, 2]} />
-          <meshStandardMaterial color="black" />
-          <textGeometry args={[frame.artText]} />
-        </mesh>
-      ))}
-
       {/* Room Sign */}
       <mesh position={[0, 2, 2]}>
         <textGeometry args={[roomName, { size: 1, height: 0.1 }]} />
@@ -56,37 +46,31 @@ function Room({ position, isActive, onClick, artFrames, roomName }) {
   );
 }
 
-// Main Gallery component
 export default function Gallery() {
   const [activeRoom, setActiveRoom] = useState(0);
   const rooms = [
-    { position: [0, 0, 0], artFrames: [{ position: [0, 0, -2], artText: "Art 1" }, { position: [0, 1, -2], artText: "Art 2" }], roomName: "Room 1" },
-    { position: [4, 0, 0], artFrames: [{ position: [0, 0, -2], artText: "Art 3" }, { position: [0, 1, -2], artText: "Art 4" }], roomName: "Room 2" },
-    { position: [-4, 0, 0], artFrames: [{ position: [0, 0, -2], artText: "Art 5" }, { position: [0, 1, -2], artText: "Art 6" }], roomName: "Room 3" },
+    { position: [0, 0, 0], roomName: "Main Room" },
+    { position: [4, 0, 0], roomName: "Room 2" },
+    { position: [-4, 0, 0], roomName: "Room 3" },
   ];
 
   return (
     <Canvas camera={{ position: [0, 2, 5] }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} />
-      
-      {/* Main entry banner */}
-      <mesh position={[0, 3, 5]}>
-        <textGeometry args={["Welcome to the Gallery", { size: 1, height: 0.1 }]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
 
-      {/* Render rooms */}
+      {/* Render Rooms */}
       {rooms.map((room, index) => (
         <Room
           key={index}
           position={room.position}
           isActive={index === activeRoom}
           onClick={() => setActiveRoom(index)}
-          artFrames={room.artFrames}
           roomName={room.roomName}
         />
       ))}
+
+      {/* OrbitControls for navigating around */}
       <OrbitControls />
     </Canvas>
   );
