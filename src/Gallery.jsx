1a35322c826b9caf64f6dfc63f8897dd90dce094
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-01-31 14:04:47 -0500

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
 
 function DebugBox() {
   return (
@@ -11,66 +10,26 @@ function DebugBox() {
     </mesh>
   );
 }
-// Gallery Room Component
+
 function GalleryRoom() {
   return (
     <group>
-      {/* Floor */}
-      <mesh position={[0, -1, 0]} receiveShadow>
-        <boxGeometry args={[10, 0.2, 10]} />
-        <meshStandardMaterial color="gray" />
-      </mesh>
-
-      {/* Walls */}
-      <mesh position={[0, 1, -5]}>
-        <boxGeometry args={[10, 3, 0.1]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      <mesh position={[0, 1, 5]}>
-        <boxGeometry args={[10, 3, 0.1]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      <mesh position={[-5, 1, 0]}>
-        <boxGeometry args={[0.1, 3, 10]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      <mesh position={[5, 1, 0]}>
-        <boxGeometry args={[0.1, 3, 10]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-
-      {/* Art Frames (You can replace these with images) */}
-      <ArtFrame position={[-2, 1, -4.9]} imageUrl={"/art/art1.jpg"} />
-      <ArtFrame position={[2, 1, -4.9]} imageUrl={"/art/art2.jpg"} />
-      <ArtFrame position={[-2, 1, 4.9]} imageUrl={"/art/art3.jpg"} />
-      <ArtFrame position={[2, 1, 4.9]} imageUrl={"/art/art4.jpg"} />
+      {/* Gallery walls, floor, ceiling (if applicable) */}
+      {/* Art frames go here */}
     </group>
   );
 }
 
-// Art Frame Component
-function ArtFrame({ position, imageUrl }) {
-  return (
-    <mesh position={position}>
-      <planeGeometry args={[2, 2]} />
-      <meshBasicMaterial map={useTexture(imageUrl)} />
-    </mesh>
-  );
-}
-
-// Load textures for images
-import { useTexture } from "@react-three/drei";
-
-// Gallery Component
 export default function Gallery() {
   return (
     <Canvas shadows camera={{ position: [0, 2, 6], fov: 50 }}>
-      <Suspense fallback={null}>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} castShadow />
-        <GalleryRoom />
-        <OrbitControls />
-      </Suspense>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} castShadow />
+      
+      <DebugBox /> {/* 🔴 Red test box should appear */}
+
+      <GalleryRoom />
+      <OrbitControls />
     </Canvas>
   );
 }
