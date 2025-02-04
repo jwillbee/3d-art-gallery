79a57f559520bbf52d001a9b79fb095016aa8f66
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-02-04 15:02:11 -0500

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -54,14 +54,13 @@ function CameraController() {
   const threshold = 20;
 
   const boundaries = [
-  // Main Hall
-  { xMin: -5, xMax: 5, zMin: 0, zMax: 75 },
-  // New Side Room on the Right
-  { xMin: 5, xMax: 15, zMin: 20, zMax: 35 }, // Adjusted zMax to 35 to cover the side room
-  // Existing Side Room on the Right
-  { xMin: 5, xMax: 15, zMin: 45, zMax: 60 },
-];
-
+    // Main Hall
+    { xMin: -5, xMax: 5, zMin: 0, zMax: 75 },
+    // Far Room on the Right
+    { xMin: 5, xMax: 15, zMin: 30, zMax: 45 },
+    // Near Room on the Right
+    { xMin: 5, xMax: 15, zMin: 5, zMax: 20 },
+  ];
 
   const [{ position, rotationY }, api] = useSpring(() => ({
     position: camera.position.toArray(),
@@ -206,105 +205,94 @@ export default function GalleryApp() {
   <Wall position={[-5, 2.5, 37.5]} rotation={[0, Math.PI / 2, 0]} size={[75, 5, 0.1]} />
   {/* Right Wall */}
   {/* Divided into three segments to create two openings for the side rooms */}
-  {/* First Segment (from z = 0 to z = 20) */}
-<Wall
-  position={[5, 2.5, 10]}
-  rotation={[0, -Math.PI / 2, 0]}
-  size={[20, 5, 0.1]}
-/>
-  {/* Second Segment (from z = 30 to z = 45) */}
-<Wall
-  position={[5, 2.5, 37.5]}
-  rotation={[0, -Math.PI / 2, 0]}
-  size={[15, 5, 0.1]}
-/>
-  {/* Third Segment (from z = 60 to z = 75) */}
-<Wall
-  position={[5, 2.5, 67.5]}
-  rotation={[0, -Math.PI / 2, 0]}
-  size={[15, 5, 0.1]}
-/>
-{/* ArtFrames */}
-{/* Left Wall */}
-{[10, 25, 40, 55, 70].map((zPos) => (
-  <ArtFrame
-    key={`left-frame-${zPos}`}
-    position={[-4.9, 2, zPos]}
-    rotation={[0, Math.PI / 2, 0]}
-  />
-))}
-{/* Right Wall */}
-{[10, 65].map((zPos) => (
-  <ArtFrame
-    key={`right-frame-${zPos}`}
-    position={[4.9, 2, zPos]}
+  {/* First Segment (from z = 0 to z = 10) */}
+  <Wall
+    position={[5, 2.5, 5]}
     rotation={[0, -Math.PI / 2, 0]}
+    size={[10, 5, 0.1]}
   />
-))}
-</group>
-
-
-
-      {/*Side Room on the Right (existing)*/}
-<group position={[10, 0, 37.5]}>
-  {/* Floor */}
-  <mesh position={[0, 0, 0]}>
-    <boxGeometry args={[10, 0.1, 15]} />
-    <meshStandardMaterial color="lightgray" />
-  </mesh>
-  {/* Ceiling */}
-  <mesh position={[0, 5, 0]}>
-    <boxGeometry args={[10, 0.1, 15]} />
-    <meshStandardMaterial color="white" />
-  </mesh>
-  {/* Walls */}
-  {/* Back Wall */}
-  <mesh position={[0, 2.5, -7.5]}>
-    <boxGeometry args={[10, 5, 0.1]} />
-    <meshStandardMaterial color="gray" />
-  </mesh>
-  {/* Front Wall */}
-  <mesh position={[0, 2.5, 7.5]}>
-    <boxGeometry args={[10, 5, 0.1]} />
-    <meshStandardMaterial color="gray" />
-  </mesh>
+  {/* Second Segment (from z = 10 to z = 30) */}
+  <Wall
+    position={[5, 2.5, 20]}
+    rotation={[0, -Math.PI / 2, 0]}
+    size={[20, 5, 0.1]}
+  />
+  {/* Third Segment (from z = 45 to z = 75) */}
+  <Wall
+    position={[5, 2.5, 60]}
+    rotation={[0, -Math.PI / 2, 0]}
+    size={[30, 5, 0.1]}
+  />
+  {/* ArtFrames */}
+  {/* Left Wall */}
+  {[10, 25, 40, 55, 70].map((zPos) => (
+    <ArtFrame
+      key={left-frame-${zPos}}
+      position={[-4.9, 2, zPos]}
+      rotation={[0, Math.PI / 2, 0]}
+    />
+  ))}
   {/* Right Wall */}
-  <mesh position={[5, 2.5, 0]}>
-    <boxGeometry args={[15, 5, 0.1]} />
-    <meshStandardMaterial color="gray" />
-  </mesh>
+  {[10, 65].map((zPos) => (
+    <ArtFrame
+      key={right-frame-${zPos}}
+      position={[4.9, 2, zPos]}
+      rotation={[0, -Math.PI / 2, 0]}
+    />
+  ))}
 </group>
 
-      {/*New Side Room on the Right*/}
+
+
+      {/* Side Room on the Right */}
+      <group position={[10, 0, 37.5]}>
+        {/* Floor */}
+        <Floor position={[0, 0, 0]} size={[10, 0.1, 15]} />
+        {/* Ceiling */}
+        <Ceiling position={[0, 5, 0]} size={[10, 0.1, 15]} />
+        {/* Walls */}
+        {/* Back Wall */}
+        <Wall position={[0, 2.5, -7.5]} rotation={[0, 0, 0]} size={[10, 5, 0.1]} />
+        {/* Front Wall */}
+        <Wall position={[0, 2.5, 7.5]} rotation={[0, Math.PI, 0]} size={[10, 5, 0.1]} />
+        {/* Right Wall */}
+        <Wall position={[5, 2.5, 0]} rotation={[0, -Math.PI / 2, 0]} size={[15, 5, 0.1]} />
+        {/* No Left Wall (open to the main hall) */}
+        {/* ArtFrames */}
+        {/* Back Wall */}
+        <ArtFrame position={[0, 2, -7.4]} rotation={[0, 0, 0]} />
+        {/* Front Wall */}
+        <ArtFrame position={[0, 2, 7.4]} rotation={[0, Math.PI, 0]} />
+        {/* Right Wall */}
+        <ArtFrame position={[4.9, 2, -5]} rotation={[0, -Math.PI / 2, 0]} />
+        <ArtFrame position={[4.9, 2, 0]} rotation={[0, -Math.PI / 2, 0]} />
+        <ArtFrame position={[4.9, 2, 5]} rotation={[0, -Math.PI / 2, 0]} />
+      </group>
+
+     {/* New Side Room closer to the start point */}
 <group position={[10, 0, 10]}>
   {/* Floor */}
-  <mesh position={[0, 0, 0]}>
-    <boxGeometry args={[10, 0.1, 15]} />
-    <meshStandardMaterial color="blue" />
-  </mesh>
+  <Floor position={[0, 0, 0]} size={[10, 0.1, 15]} />
   {/* Ceiling */}
-  <mesh position={[0, 5, 0]}>
-    <boxGeometry args={[10, 0.1, 15]} />
-    <meshStandardMaterial color="yellow" />
-  </mesh>
+  <Ceiling position={[0, 5, 0]} size={[10, 0.1, 15]} />
   {/* Walls */}
   {/* Back Wall */}
-  <mesh position={[0, 2.5, -7.5]}>
-    <boxGeometry args={[10, 5, 0.1]} />
-    <meshStandardMaterial color="red" />
-  </mesh>
+  <Wall position={[0, 2.5, -7.5]} rotation={[0, 0, 0]} size={[10, 5, 0.1]} />
   {/* Front Wall */}
-  <mesh position={[0, 2.5, 7.5]}>
-    <boxGeometry args={[10, 5, 0.1]} />
-    <meshStandardMaterial color="white" />
-  </mesh>
+  <Wall position={[0, 2.5, 7.5]} rotation={[0, Math.PI, 0]} size={[10, 5, 0.1]} />
   {/* Right Wall */}
-  <mesh position={[5, 2.5, 0]}>
-    <boxGeometry args={[15, 5, 0.1]} />
-    <meshStandardMaterial color="green" />
-  </mesh>
+  <Wall position={[5, 2.5, 0]} rotation={[0, -Math.PI / 2, 0]} size={[15, 5, 0.1]} />
+  {/* No Left Wall (open to the main hall) */}
+  {/* ArtFrames */}
+  {/* Back Wall */}
+  <ArtFrame position={[0, 2, -7.4]} rotation={[0, 0, 0]} />
+  {/* Front Wall */}
+  <ArtFrame position={[0, 2, 7.4]} rotation={[0, Math.PI, 0]} />
+  {/* Right Wall */}
+  <ArtFrame position={[4.9, 2, -5]} rotation={[0, -Math.PI / 2, 0]} />
+  <ArtFrame position={[4.9, 2, 0]} rotation={[0, -Math.PI / 2, 0]} />
+  <ArtFrame position={[4.9, 2, 5]} rotation={[0, -Math.PI / 2, 0]} />
 </group>
-
 
     </Canvas>
   );
