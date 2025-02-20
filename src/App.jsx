4f6dbbd3169da17a0ce23a3276cce805571700b2
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-02-20 14:21:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Gallery from "./Gallery";
 
-export default function App() {
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Gallery />
-    </div>
-  );
-}
+const App = () => <Gallery />;
+
+export default App;
+
