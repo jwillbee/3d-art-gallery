18225b97a2176361dd91f90558be1b0e5aad4e63
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-02-20 10:22:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "", // Change this to "" (empty) or remove it entirely
+  base: "/", // Change this to "" (empty) or remove it entirely
   build: {
     outDir: "dist"
   }
